b4f8047fd74494c182b1e083a8d30f5c611f574b
Riyadh Uddin <noreplythnx@gmail.com>
2021-10-19 12:11:15 +0600

    Add files via upload

---
+++ mainReward.py
@@ -0,0 +1,84 @@
+import math
+def reward_function(params):
+    '''
+    rewarding the agent to stay inside two borders
+    and penalizing getting too close to the objects in front and steering too much.
+    '''
+
+    all_wheels_on_track = params['all_wheels_on_track']
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+    objects_distance = params['objects_distance']
+    _, next_object_index = params['closest_objects']
+    objects_left_of_center = params['objects_left_of_center']
+    is_left_of_center = params['is_left_of_center']
+    
+    
+
+    # Initialize reward with a small number but not zero
+    # because zero means off-track or crashed
+    reward = 1e-3
+
+    # Reward if the agent stays inside the two borders of the track
+    if all_wheels_on_track and (0.5 * track_width - distance_from_center) >= 0.05:
+        reward_lane = 1.0
+    else:
+        reward_lane = 1e-3
+
+    # Penalize if the agent is too close to the next object
+    reward_avoid = 1.0
+    
+    #custom
+    steering = params['steering_angle']
+    throttle = params['speed']
+     # Read input variables
+    waypoints = params['waypoints']
+    closest_waypoints = params['closest_waypoints']
+    heading = params['heading']
+    # Calculate the direction of the center line based on the closest waypoints
+    next_point = waypoints[closest_waypoints[1]]
+    prev_point = waypoints[closest_waypoints[0]]
+    #borders
+    distance_from_border = 0.5 * track_width - distance_from_center
+    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
+    track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
+    # Convert to degree
+    track_direction = math.degrees(track_direction)
+    # Calculate the difference between the track direction and the heading direction of the car
+    direction_diff = abs(track_direction - heading)
+    if direction_diff > 180:
+        direction_diff = 360 - direction_diff
+    
+    #custom
+    rewardMe = 1e-3
+    # Reward higher if the car stays inside the track borders
+    if distance_from_border >= 0.05:
+        rewardMe = 1.0
+     # Penalize the reward if the difference is too large
+    DIRECTION_THRESHOLD = 10.0
+    if direction_diff > DIRECTION_THRESHOLD:
+        rewardMe *= 0.5
+    if throttle > 2.5:
+        rewardMe *= 1
+    if abs(steering) > 0.3:
+        rewardMe *= 0.80
+    #custom speed waypoints
+    
+    # Distance to the next object
+    distance_closest_object = objects_distance[next_object_index]
+    # Decide if the agent and the next object is on the same lane
+    is_same_lane = objects_left_of_center[next_object_index] == is_left_of_center
+
+    if is_same_lane:
+        if 0.5 <= distance_closest_object < 0.8: 
+            reward_avoid *= 0.5
+        elif 0.3 <= distance_closest_object < 0.5:
+            reward_avoid *= 0.2
+        elif distance_closest_object < 0.3:
+            reward_avoid = 1e-3 # Likely crashed
+
+    # Calculate reward by putting different weights on 
+    # the two aspects above
+    reward += 1.0 * reward_lane + 4.0 * reward_avoid + 1.0 * rewardMe
+
+    return reward
